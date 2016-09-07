565663d6e007ef3f85ecf5ea9f09b53e04f8e5ae
Chion Tang <sdspeedonion@gmail.com>
2016-09-07 23:08:28 +0800

    Fix RefreshControl style

--- src/components/RefreshControl/index.js
+++ src/components/RefreshControl/index.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import ReactPullToRefresh from '../../modules/react-pull-to-refresh/src'
 import ActivityIndicator from '../ActivityIndicator'
+import _ from 'lodash'
 
 const arrowIcon = `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAQAAABKfvVzAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QAAKqNIzIAAAAJcEhZcwAADdcAAA3XAUIom3gAAAAHdElNRQfgCQYHLCTylhV1AAAAjklEQVQ4y2P8z0AaYCJRPX4NsyNWM5Ok4R/n+/noWhjx+2F20n8HwcTQv0T7IXUe4wFUWwh6Gl0LEaGEqoWoYEXWQmQ8ILQwEh/TkBBjme3HIESkjn+Mv9/vJjlpkOwkom2AxTmRGhBJhCgNyCmKCA2oCZCgBvT0ykSacgIaZiaiKydoA7pykiKOSE+jAwADZUnJjMWwUQAAACV0RVh0ZGF0ZTpjcmVhdGUAMjAxNi0wOS0wNlQwNzo0NDozNiswMjowMAZN3oQAAAAldEVYdGRhdGU6bW9kaWZ5ADIwMTYtMDktMDZUMDc6NDQ6MzYrMDI6MDB3EGY4AAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAAABJRU5ErkJggg==`
 const css = `.ptr-element .pull-down-icon {
-	font-size: 34px;
-	transition: all .25s ease;
-	-webkit-transform: rotate(90deg);
-	transform: rotate(90deg);
-	margin-top: -22px;
+  font-size: 34px;
+  transition: all .25s ease;
+  -webkit-transform: rotate(90deg);
+  transform: rotate(90deg);
+  margin-top: -22px;
   margin-left: auto;
   margin-right: auto;
   display: block;
@@ -16,23 +17,23 @@ const css = `.ptr-element .pull-down-icon {
   height: 22px;
 }
 .ptr-refresh .pull-down-icon {
-	-webkit-transform: rotate(270deg);
-	transform: rotate(270deg);
+  -webkit-transform: rotate(270deg);
+  transform: rotate(270deg);
 }
 .ptr-loading .pull-down-icon {
   display: none;
 }
 .ptr-reset .pull-down-icon {
-	display: block;
+  display: block;
 }
 
 .ptr-element .loading {
-	display: inline-block;
-	display: none;
+  display: inline-block;
+  display: none;
   margin-top: 5px;
 }
 .ptr-loading .loading {
-	display: block;
+  display: block;
   margin-bottom: 10px;
 }`;
 
@@ -47,7 +48,8 @@ class RefreshControl extends Component {
   static propTypes = {
     onRefresh: React.PropTypes.func.isRequired,
     refreshing: React.PropTypes.bool.isRequired,
-    children: React.PropTypes.any
+    children: React.PropTypes.any,
+    style: React.PropTypes.any
   }
 
   handleRefresh(resolve) {
@@ -60,11 +62,15 @@ class RefreshControl extends Component {
   }
 
   render() {
+    const style = _.cloneDeep(this.props.style[2] || {})
+    style.overflowY = 'auto'
+
     return (
       <ReactPullToRefresh
-        onRefresh={this.handleRefresh.bind(this)}
         icon={<img width={22} height={22} src={arrowIcon} className="pull-down-icon" />}
-        loading={<div className="loading"><ActivityIndicator animating={true} /></div>}>
+        loading={<div className="loading"><ActivityIndicator animating={true} /></div>}
+        onRefresh={this.handleRefresh.bind(this)}
+        style={style}>
         {this.props.children}
       </ReactPullToRefresh>
     )
